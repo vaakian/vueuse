09516d4e27af93f10ec0023cba1375f5dc257dc7
vaakian <vaakian@gmail.com>
2022-09-08 23:02:52 +0800

    fix: vue2 compatible

--- packages/shared/computedWithControl/index.ts
+++ packages/shared/computedWithControl/index.ts
@@ -1,6 +1,5 @@
 import type { ComputedGetter, ComputedRef, WatchSource, WritableComputedOptions, WritableComputedRef } from 'vue-demi'
-import { customRef, ref, watch } from 'vue-demi'
-import type { Fn } from '../utils'
+import { computed, ref, watch } from 'vue-demi'
 import { isFunction } from '../utils'
 
 export interface ComputedWithControlRefExtra {
@@ -34,41 +33,31 @@ export function computedWithControl<T, S>(
   fn: ComputedGetter<T> | WritableComputedOptions<T>,
 ) {
   let v: T = undefined!
-  let track: Fn
-  let trigger: Fn
-  const dirty = ref(true)
-
-  const update = () => {
-    dirty.value = true
-    trigger()
-  }
-
-  watch(source, update, { flush: 'sync' })
 
   const get = isFunction(fn) ? fn : fn.get
   const set = isFunction(fn) ? undefined : fn.set
 
-  const result = customRef<T>((_track, _trigger) => {
-    track = _track
-    trigger = _trigger
+  const flag = ref(true)
+  const update = () => {
+    v = get()
+    // notify getter
+    flag.value = !flag.value
+  }
 
-    return {
-      get() {
-        if (dirty.value) {
-          v = get()
-          dirty.value = false
-        }
-        track()
-        return v
-      },
-      set(v) {
-        set?.(v)
-      },
-    }
+  watch(source, update, { flush: 'sync', immediate: true })
+
+  const result = computed({
+    get() {
+      // eslint-disable-next-line no-unused-expressions
+      flag.value
+      return v
+    },
+    set(v) {
+      set?.(v)
+    },
   }) as ComputedRefWithControl<T>
 
-  if (Object.isExtensible(result))
-    result.trigger = update
+  result.trigger = update
 
   return result
 }
